ccf68a25e6c919a7e1a7395691629b5ddaf30566
Valentin Lenk <valentin.lenk@gmail.com>
2022-04-28 16:20:14 +0200

    Rename tests directory to reflect test intentions

---
+++ run_static_output_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# The scripts in static_output_tests are intended only to statically verify the build output files.
+# No guarantee can be made that a server will be running, and no changes to the files are to be made.
+
+set -e
+
+echo "Running static output tests from $PWD"
+
+for f in static_ouput_tests/*.sh; do
+  bash "$f"
+done
--- run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running tests from $PWD"
-
-for f in tests/*.sh; do
-  bash "$f"
-done
